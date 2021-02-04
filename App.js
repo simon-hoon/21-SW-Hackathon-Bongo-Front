151b4afa637dec858e72c68d6e3ba1dec378b012
SiyeolBaek <qortlduf505@gmail.com>
2021-02-05 04:34:01 +0900

    폰트 추가 및 버튼 오류 해결

--- App.js
+++ App.js
@@ -1,47 +1,18 @@
 import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, Button, SafeAreaView, Alert ,Image} from 'react-native';
+import { StyleSheet, Text, View, Button, SafeAreaView, Alert ,Image, TouchableOpacity} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
-function HomeScreen() {
-  return (
-    <View style={styles.container}>
-      <Image style={styles.logoContainer} source={require("./src/images/logoWHite.svg")} />
-      <View style={styles.signUp}>
-      <Button style ={styles.signUpLayer}
-        title="회원 가입"
-        onPress={() => Alert.alert('Simple Button pressed')}
-      />
-      </View>
-      <View style={styles.login}>
-      <Button style ={styles.loginLayer}
-        title="로그인"
-        onPress={() => Alert.alert('Simple Button pressed')}
-      />
-      </View>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const Stack = createStackNavigator();
-
-export default function App() {
-  return (    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-}
+import logo from './src/images/logoWhite.svg';
 
 const colors = {
   blue: "#0036d5",
   flatBlueSkyLight: "#fdfeff",
   lightGray: "#d0d0d0",
-  signatureBlue: "#0047cc"
+  signatureBlue: "#0047cc",
+  white: "#ffffff"
 };
 
 const loginCopy = {
@@ -59,31 +30,31 @@ const login = {
   borderRadius: 27.6,
   borderStyle: "solid",
   borderWidth: 1.7,
-  backgroundColor: colors.white
+  borderColor: colors.white,
+  backgroundColor: colors.white,
+  marginTop: 15
 };
 
 const signUpLayer = {
-  width: 142,
-  height: 26,
   fontFamily: "NotoSansCJKkr",
   fontSize: 17.7,
   fontWeight: "bold",
   fontStyle: "normal",
   letterSpacing: 0.44,
   textAlign: "center",
-  color: colors.white
+  color: colors.white,
+  paddingTop: 12
 };
 
 const loginLayer = {
-  width: 286,
-  height: 26,
   fontFamily: "NotoSansCJKkr",
   fontSize: 17.7,
   fontWeight: "bold",
   fontStyle: "normal",
   letterSpacing: 0.44,
   textAlign: "center",
-  color: colors.blue
+  color: colors.blue,
+  paddingTop: 12
 };
 
 const styles = StyleSheet.create({
@@ -97,10 +68,38 @@ const styles = StyleSheet.create({
     width: 200,
     height: 200
   },
+  test: {
+    width: 612,
+  },
   signUp: loginCopy,
   signUpLayer : signUpLayer,
   loginLayer : loginLayer,
-  login: login, 
+  login: login,
 });
 
 
+function HomeScreen() {
+  return (
+    <View style={styles.container}>
+      <Image style={styles.logoContainer} source={logo} />
+      <TouchableOpacity style={styles.signUp}>
+        <Text style={styles.signUpLayer}>회원가입</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.login}>
+        <Text style={styles.loginLayer}>로그인</Text>
+      </TouchableOpacity>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
